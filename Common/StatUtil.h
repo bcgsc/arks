c51f1b3adc85eea8fe007caa7e1381fba3b49483
Ben Vandervalk <benv@bcgsc.ca>
2017-10-30 12:34:15 -0700

    StatUtil: replace `median`/`IQR` with `quantile` method
    The new `quantile` method is simple and more generic.

--- Common/StatUtil.h
+++ Common/StatUtil.h
@@ -2,57 +2,31 @@
 #define _STAT_UTIL_H_ 1
 
 #include <algorithm>
+#include <cassert>
 
-/** Calculate the median of an unsorted container */
-template <typename IteratorT>
-static inline float median(const IteratorT& it1,
-	const IteratorT& it2)
+template <class IteratorT>
+static inline double quantile(IteratorT it1, IteratorT it2, double q)
 {
-	size_t size = it2 - it1;
-	if (size == 0)
-		return 0.0f;
+	assert(q >= 0.0);
+	assert(q <= 1.0);
 
-	if (size % 2 == 0) {
-		/* return average of middle two elements */
-		IteratorT mid1 = it1 + size / 2 - 1;
-		IteratorT mid2 = it1 + size / 2;
-		std::nth_element(it1, mid1, it2);
-		std::nth_element(it1, mid2, it2);
-		return float(*mid1 + *mid2) / 2.0f;
-	} else {
-		/* return middle element */
-		IteratorT mid = it1 + size / 2;
-		std::nth_element(it1, mid, it2);
-		return float(*mid);
-	}
-}
+	size_t length = it2 - it1;
+	assert(length >= 1);
 
-/** Calculate the inter-quartile range of an unsorted container */
-template <typename IteratorT>
-static inline
-std::pair<typename IteratorT::value_type, typename IteratorT::value_type>
-IQR(const IteratorT& it1, const IteratorT& it2)
-{
-	typedef typename IteratorT::value_type value_type;
+	size_t lastPos = length - 1;
 
-	size_t size = it2 - it1;
-	if (size == 0)
-		return std::make_pair(0, 0);
-	if (size == 1)
-		return std::make_pair(*it1, *it1);
+	/* get elements bordering quantile boundary */
 
-	value_type q1, q3;
-	IteratorT mid = it1 + size / 2;
+	size_t beforePos = (size_t)floor(q * lastPos);
+	size_t before = *(it1 + beforePos);
 
-	if (size % 2 == 0) {
-		q1 = median(it1, mid);
-		q3 = median(mid, it2);
-	} else {
-		q1 = median(it1, mid + 1);
-		q3 = median(mid, it2);
-	}
+	size_t afterPos = (size_t)ceil(q * lastPos);
+	size_t after = *(it1 + afterPos);
 
-	return std::make_pair(q1, q3);
+	/* weight elements by distance to quantile boundary */
+
+	double weight = (q * lastPos - beforePos) / 1.0;
+	return weight * before + (1.0 - weight) * after;
 }
 
 #endif
