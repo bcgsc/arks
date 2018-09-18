2809a7fb47de46ae32a1ce4328136d8f8ffdddd3
lcoombe <lauren.e.coombe@gmail.com>
2018-09-18 14:53:25 -0700

    arks-make: Update time/memory logging

--- Examples/arks_test-demo/runARKSdemo.sh
+++ Examples/arks_test-demo/runARKSdemo.sh
@@ -3,4 +3,4 @@
 echo "Ensure the following are in your PATH: arks, LINKS, arks-make"
 echo "	NOTE: arks-make Makefile is found in Examples directory of repo"
 echo Running ARKS makefile....
-arks-make arks draft=test_scaffolds reads=test_reads m=50-6000 a=0.9
+arks-make arks draft=test_scaffolds reads=test_reads m=50-6000 a=0.9 time=1 
